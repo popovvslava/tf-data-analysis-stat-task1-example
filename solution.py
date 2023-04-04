dc57390c9dfec1631a3d37a15e5fd32887d1dc7d
popovvslava <129688432+popovvslava@users.noreply.github.com>
2023-04-04 08:43:41 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import scipy as sc
-https://github.com/popovvslava/tf-data-analysis-stat-task1-example/blob/main/solution.py
 
 chat_id = 368379249 # Ваш chat ID, не меняйте название переменной
 
