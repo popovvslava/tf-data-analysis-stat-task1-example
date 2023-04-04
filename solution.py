206b82616567a97d8f685e495a4bd4835ecbbb49
popovvslava <129688432+popovvslava@users.noreply.github.com>
2023-04-04 19:28:38 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,6 +5,7 @@ import scipy as sc
 chat_id = 368379249 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
+    x = x - 645
     mean_val = x.mean()
     val = np.log(mean_val**2/np.sqrt(mean_val**2+sc.stats.tstd(x)))
-    return val 
+    return val
